f679a2fd7bc543e40163772500836a88407827d8
rekofke <rekofke.eric@gmail.com>
2025-03-09 04:56:53 -0700

    created file of JavaScript design ideas scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,72 @@
-function Enter() {
-    let caption = document.getElementById("caption");
-    caption.innerHTML = "You hovered over the button.";
+// 1. Random design ideas:
+
+const designIdeas = {
+    "Consistency creates trust",
+    "Contrast is key",
+    "White space is necessary",
+    "Mobile first"
+};
+
+function getRandomIdea() {
+    return designIdeas[Math, floor(Math.random() * designIdeas.length)];
 }
 
-function Exit() {
-    let caption = document.getElementById("caption");
-    caption.innerHTML = "You hovered out the button.";
+// 2. Interactive checklist:
+
+const featuresList = {
+    { idea: 1, text: "Create dark mode", done: false },
+    { idea: 2, text: "Add Carousoul or accordian", done: false },
+    { idea: 3, text: "Add avatars for 'meet the team'", done: true },
+    
+};
+
+function toggleFeature(idea) {
+    const feature = teaturesList.find(item => item.idea === idea);
+    if (feature) feature.done = !feature.done;
+    return featuresList;
 }
 
-function GreetMe() {
-    console.log("Hello world.");
+// 3. Progress calculator
+
+function showProgress(current, total) {
+    const percentage = (current / total * 100).toFixed(1);
+    return `Progress: ${current}/${total} (${percentage}%)`;
+}
+
+console.log(showProgress(3, 10)); //<--- 30.0%
+
+
+// 4. Enhance form input
+
+function formatUsername(input) {
+    return input.trim().toLowerCase().replace(/\s+/g, '-');
+}
+
+const userInput = " Code Master ";
+console.log(formatUsername(userInput));
+
+// 5. Greeting generator
+
+const greetings = ["Welcome back", "So glad you returned", "Great to see you", "Hello!"];
+const username = "CodeMaster"
+
+function showGreeting() {
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return `${randomGreeting}, ${username}! Lets make something great!`;
+}
+
+// 6. Carousel of company features
+
+const features = {
+    "Full stack design",
+    "Cloud solutions",
+    "Mobile app design",
+    "On-site classes"
+};
+
+let currentFeature = 0;
+
+function cycleFeature() {
+    currentFeature = (currentFeature + 1) % features length;
+    document.getElementById('feature-display').textContent = features[currentFeature];
 }
